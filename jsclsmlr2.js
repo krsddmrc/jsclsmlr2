71fd6762288be476542dbab66efdf2d9fcb1de59
k2rdnz <k2rdnz@gmail.com>
2022-02-23 13:48:48 +0300

    "yeni değişiklik"

---
+++ jsclsmlr2.js
@@ -0,0 +1,16 @@
+/* var x = 99999999999999999999999999999999999999;
+var y = 9999999999999999999999999;
+z = x + y;
+console.log(z); */
+
+/* var a = 10;
+console.log(a++);
+console.log(a);
+console.log(--a);
+console.log(--a);
+var c = 5;
+c = --c
+console.log({ a }, { c });
+var d = 8;
+console.log(d++ + ++d); */
+/* console.log(null && NaN); */
